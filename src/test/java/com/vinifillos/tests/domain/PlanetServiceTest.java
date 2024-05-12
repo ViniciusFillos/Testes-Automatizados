8d337564a30480d23d579bea7f95a160f1c59b53
Vinicius Fillos <vinifillos@gmail.com>
2024-05-12 09:35:18 -0300

    test: implementado teste para criação inválida de planet

--- src/test/java/com/vinifillos/tests/domain/PlanetServiceTest.java
+++ src/test/java/com/vinifillos/tests/domain/PlanetServiceTest.java
@@ -1,6 +1,7 @@
 package com.vinifillos.tests.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static com.vinifillos.common.PlanetConstants.INVALID_PLANET;
 import static com.vinifillos.common.PlanetConstants.PLANET;
 import static org.mockito.Mockito.when;
 
@@ -31,5 +33,12 @@ public class PlanetServiceTest {
         Planet sut = planetService.create(PLANET);
 
         assertThat(sut).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException() {
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
     }
 }
