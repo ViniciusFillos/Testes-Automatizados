63fd1d3cdcf6435b60e00dacb9a79d3e9d041f82
Vinicius Fillos <vinifillos@gmail.com>
2024-05-14 10:10:12 -0300

    test: arquivo sql para testes adicionado

---
+++ src/test/java/com/vinifillos/common/import_planets.sql
@@ -0,0 +1,3 @@
+INSERT INTO planets(id, name, cliamte, terrain) VALUES (1, "Tatooine", "arid", "desert");
+INSERT INTO planets(id, name, cliamte, terrain) VALUES (2, "Alderan", "temperate", "grassland, mountains");
+INSERT INTO planets(id, name, cliamte, terrain) VALUES (3, "Yavin IV", "temperate, tropical", "jungle, rainforests");
