9dacdda801b372ee36a13fca68c60c4e8a8de032
Vinicius Fillos <vinifillos@gmail.com>
2024-05-14 10:05:04 -0300

    test: adicinado novas constantes para testes

--- src/test/java/com/vinifillos/common/PlanetConstants.java
+++ src/test/java/com/vinifillos/common/PlanetConstants.java
@@ -2,8 +2,20 @@ package com.vinifillos.common;
 
 import com.vinifillos.tests.domain.Planet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlanetConstants {
     public static final Planet PLANET = new Planet("name", "climate", "terrain");
     public static final Planet INVALID_PLANET = new Planet("", "", "");
     public static final Planet EMPITY_PLANET = new Planet();
+
+    public static final Planet TATOOINE = new Planet("Tatooine", "arid", "desert");
+    public static final Planet ALDERAN = new Planet("Alderan", "temperate", "grassland, mountains");
+    public static final Planet YAVINIV = new Planet("Yavin IV", "temperate, tropical", "jungle, rainforests");
+    public static final List<Planet> PLANETS = new ArrayList<>() {{
+        add(TATOOINE);
+        add(ALDERAN);
+        add(YAVINIV);
+    }};
 }
