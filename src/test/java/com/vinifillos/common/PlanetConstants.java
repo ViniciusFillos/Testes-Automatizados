13f9995c4621e0ca407b8c8a7588f20b4335a8d9
Vinicius Fillos <vinifillos@gmail.com>
2024-05-14 11:01:22 -0300

    test: constantes para testes mudadas

--- src/test/java/com/vinifillos/common/PlanetConstants.java
+++ src/test/java/com/vinifillos/common/PlanetConstants.java
@@ -10,12 +10,15 @@ public class PlanetConstants {
     public static final Planet INVALID_PLANET = new Planet("", "", "");
     public static final Planet EMPITY_PLANET = new Planet();
 
-    public static final Planet TATOOINE = new Planet("Tatooine", "arid", "desert");
-    public static final Planet ALDERAN = new Planet("Alderan", "temperate", "grassland, mountains");
-    public static final Planet YAVINIV = new Planet("Yavin IV", "temperate, tropical", "jungle, rainforests");
-    public static final List<Planet> PLANETS = new ArrayList<>() {{
-        add(TATOOINE);
-        add(ALDERAN);
-        add(YAVINIV);
-    }};
+    public static final Planet TATOOINE = new Planet(1L, "Tatooine", "arid", "desert");
+    public static final Planet ALDERAAN = new Planet(2L, "Alderaan", "temperate", "grasslands, mountains");
+    public static final Planet YAVINIV = new Planet(3L, "Yavin IV", "temperate, tropical", "jungle, rainforests");
+    public static final List<Planet> PLANETS = new ArrayList<>() {
+        {
+            add(TATOOINE);
+            add(ALDERAAN);
+            add(YAVINIV);
+        }
+    };
 }
+
