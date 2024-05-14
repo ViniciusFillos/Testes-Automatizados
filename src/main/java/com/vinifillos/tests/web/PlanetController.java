21cff99ade0066cbe95587b37075489871c493d2
Vinicius Fillos <vinifillos@gmail.com>
2024-05-14 11:05:13 -0300

    test: adicionado test para repository list

--- src/main/java/com/vinifillos/tests/web/PlanetController.java
+++ src/main/java/com/vinifillos/tests/web/PlanetController.java
@@ -6,7 +6,6 @@ import com.vinifillos.tests.domain.Planet;
 import com.vinifillos.tests.domain.PlanetService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-
 
 @RestController
 @RequestMapping("/planets")
