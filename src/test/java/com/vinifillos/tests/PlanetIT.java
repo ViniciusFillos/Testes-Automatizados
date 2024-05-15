d6992ea364b5cd824ac96a1b5b835706abfbb0e0
Vinicius Fillos <vinifillos@gmail.com>
2024-05-15 08:53:07 -0300

    test: teste de integração para create planet retornando created implementado

--- src/test/java/com/vinifillos/tests/PlanetIT.java
+++ src/test/java/com/vinifillos/tests/PlanetIT.java
@@ -1,15 +1,31 @@
 package com.vinifillos.tests;
 
+import com.vinifillos.tests.domain.Planet;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+
+import static com.vinifillos.tests.common.PlanetConstants.PLANET;
 
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PlanetIT {
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
-    void contextLoads() {
-    }
+    void createPlanet_ReturnsCreated() {
+        ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
 
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(sut.getBody().getId()).isNotNull();
+        assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
+        assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+    }
 }
